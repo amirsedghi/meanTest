64ffa9289d84e9538839e0bf1788b12bc71cb246
amirsedghi <amir.sepultura@gmail.com>
2016-09-23 04:45:06 -0700

    first commit

---
+++ client/static/factories/dashboardFactory.js
@@ -0,0 +1,11 @@
+app.factory('dashboardFactory', ['$http', '$location', function($http, $location){
+  // var dashboardObject = {};
+  //
+  // $http({
+  //   method: "GET",
+  //   url: '/dashboard'
+  // }).then(function(data){
+  //   console.log(data);
+  // })
+  // return dashboardObject;
+}])
---
+++ server/controllers/Users.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User');//change example to what you desire
+module.exports = {
+  register: function(req,res){
+    var user = new User(req.body);
+    user.save(function(err){
+      if (err){
+        res.sendStatus(500);
+      } else{
+        req.session.user = user;
+        console.log(user);
+        res.send(user);
+      }
+    });
+  },
+
+  login: function(req, res){
+    User.findOne({email:req.body.email}).exec(function(err, user){
+      if (req.body.password == user.password){
+        req.session.user = user;
+        console.log(user);
+        res.send(user);
+      } else{
+        res.sendStatus(400);
+      }
+    });
+  },
+  logout: function(req,res){
+    req.session.destroy();
+    res.sendStatus(200);
+  },
+  getuser: function(req,res){
+    User.findOne({_id:req.session.user._id}, function(err, user){
+      if (err){
+        console.log('Could not find user');
+        res.sendStatus(400);
+      } else{
+        console.log('Good shit user');
+        res.json(user);
+      }
+    })
+  }
+}
